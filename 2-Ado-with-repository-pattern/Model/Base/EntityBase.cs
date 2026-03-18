68e963c713931728609980c4034878a074595b60
Charles Anderson <andersc@wou.edu>
2026-03-18 10:45:37 -0700

    Add comment about the primary key in EntityBase

--- 2-Ado-with-repository-pattern/Model/Base/EntityBase.cs
+++ 2-Ado-with-repository-pattern/Model/Base/EntityBase.cs
@@ -1,5 +1,9 @@
 namespace Ado.Model.Base;
 public abstract class EntityBase
 {
+    // Note: this assumes that every model has a primary key called Id,
+    // which is what the SQL schema created. If you wanted to primary keys
+    // with names like EmployeeId and DepartmentId, you probably wouldn't
+    // have this attribute here, and the base class might be empty.
     public int Id { get; set; }
 }
